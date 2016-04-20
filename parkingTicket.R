228028fc2680f2654f7c0635f3d54b348035fce1
ILIKEFUUD <rahulpshah@live.com>
2016-04-20 13:26:58 -0400

    Started the ticket project
    http://simplystatistics.org/2012/10/04/statistics-project-ideas-for-students-part-2/
    
    Use city data to predict areas with the highest risk for parking tickets

---
+++ parkingTicket.R
@@ -0,0 +1,10 @@
+#Use city data to predict areas with the highest risk for parking tickets
+
+#based on http://simplystatistics.org/2012/10/04/statistics-project-ideas-for-students-part-2/
+
+data <- read.csv("Parking_citations.csv")
+
+address <- data$address
+
+
+
